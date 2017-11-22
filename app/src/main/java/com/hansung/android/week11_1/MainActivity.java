722ace02faa5edf23771ca55bcc0e9a2cb2bc290
pyjyjyj <vpfpzk@naver.com>
2017-11-22 17:15:26 +0900

    실습완성

--- app/src/main/java/com/hansung/android/week11_1/MainActivity.java
+++ app/src/main/java/com/hansung/android/week11_1/MainActivity.java
@@ -2,12 +2,16 @@ package com.hansung.android.week11_1;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,15 +21,22 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
     private FusedLocationProviderClient mFusedLocationClient;
     private Location mLastLocation;
     GoogleMap mGoogleMap = null;
     final private int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 100;
+    final private String TAG = "LocationService";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         findbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-
+                getAddress();
             }
         });
 
@@ -86,5 +97,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},        // 요청할 권한 목록을 설정한 String 배열
                 requestCode    // 사용자 정의 int 상수. 권한 요청 결과를 받을 때
         );
+    }
+
+    private void getAddress() {
+        TextView addressTextView = (TextView) findViewById(R.id.result);
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+
+            EditText place = (EditText)findViewById(R.id.place);
+            TextView result = (TextView)findViewById(R.id.result);
+            List<Address> addresses = geocoder.getFromLocationName(place.getText().toString(),1);
+            if(addresses.size()>0){
+                Address bestResult = (Address) addresses.get(0);
+
+                result.setText(String.format("[%s, %s]",
+                        bestResult.getLatitude(),
+                        bestResult.getLongitude()));
+            }
+
+            LatLng location2= new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
+
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location2,15));
+
+            mGoogleMap.addMarker(
+                    new MarkerOptions().
+                            position(location2).
+                            title(place.getText().toString()).
+                            alpha(0.8f).
+                            icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow))
+            );
+        } catch (IOException e) {
+            Log.e(TAG, "Failed in using Geocoder",e);
+            return;
+        }
+
     }
 }
